2f2bc2ad6581e6539dd214d875faa8c1ff42f244
alonso1298 <alonsosagrerogranados@gmail.com>
2025-04-03 11:41:45 -0600

    Se agrega el componente button

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import useFormulario from './hooks/useFormulario';
 import Input from './components/Input';
 import Card from './components/Card';
 import Container from './components/Container';
+import Button from './components/Button';
 
 function App() {
-  const [formulario, handleChange] = useFormulario({name: '', lastname: ''});
+  const [formulario, handleChange] = useFormulario({
+    name: '', 
+    email: '',
+    lastname: '', 
+  });
 
   console.log(formulario);
   return (
@@ -24,6 +29,13 @@ function App() {
               value={formulario.lastname} 
               onChange={handleChange} 
             />
+            <Input 
+              label='Correo' 
+              name='email' 
+              value={formulario.email} 
+              onChange={handleChange} 
+            />
+            <Button>Enviar</Button>
           </form>
         </div>
       </Card>
